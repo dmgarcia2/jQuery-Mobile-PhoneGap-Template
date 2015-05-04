d3b6c9ef8dd11f136cb1faea2e4772f826acaa4b
David Martín García <dmgarcia2@gmail.com>
2015-05-05 00:39:02 +0200

    Initial import

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var phonegap = {};
+
+phonegap.app = {
+	
+    initialize: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+		FastClick.attach(document.body);
+    }
+
+};
